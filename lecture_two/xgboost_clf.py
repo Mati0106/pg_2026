37464520bde414c2a2b8c97fb2dc9b19801f206e
Mati0106 <mateusz.serockiii@gmail.com>
2026-01-17 12:13:01 +0100

    Update xgboost_clf.py

--- lecture_two/xgboost_clf.py
+++ lecture_two/xgboost_clf.py
@@ -13,7 +13,7 @@ seed = 7
 test_size = 0.33
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=test_size, random_state=seed)
 
-model = XGBClassifier()
+model = XGBClassifier(max_depth=5, gamma=10)
 model.fit(X_train, y_train)
 
 print(model)
